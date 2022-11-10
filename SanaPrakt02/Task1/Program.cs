65c5f59dcca0d348e82cf22b7d71d346ef4e58da
Dmytro Shabotynkiy <117020644+ShabotynskiyDmytro@users.noreply.github.com>
2022-11-10 21:44:41 +0200

    3 line TEXT

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Цей текст написано у гілці ТЕКСТ");
+Console.WriteLine("І цей текст написано у гілці ТЕКСТ");
+Console.WriteLine("І цей текст також написано у гілці ТЕКСТ");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
